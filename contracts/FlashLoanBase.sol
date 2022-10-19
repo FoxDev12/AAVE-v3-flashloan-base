92dac9ec3b0b79ee9f79de5a12c9564e72d597d0
Nova <83777181+FoxDev12@users.noreply.github.com>
2022-10-19 02:45:15 +0200

    Update FlashLoanBase.sol
    Fix style

--- contracts/FlashLoanBase.sol
+++ contracts/FlashLoanBase.sol
@@ -4,11 +4,13 @@ import "./interfaces/IFlashloanSimpleReceiver.sol";
 import "./interfaces/IERC20.sol";
 
 contract FlashLoanBase is IFlashLoanSimpleReceiver {
+
     error NotEnoughFundsToRepayFlashloan(uint balance, uint wanted);
     error UnauthorizedInitiator(address initiator, address wanted);
     error SenderIsNotPool(address sender, address wanted);
     error NotOwner(address sender, address owner);
     error NotAdmin(address sender);
+
     IPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
     IPool public immutable override POOL;
     IERC20 public toBorrow;
@@ -30,13 +32,13 @@ contract FlashLoanBase is IFlashLoanSimpleReceiver {
         }
         _;
     }
+    
     constructor(IPoolAddressesProvider provider, IERC20 _toBorrow) {
         ADDRESSES_PROVIDER = provider;
         POOL = IPool(provider.getPool());
         owner = msg.sender;
         admins[msg.sender] = true;
         toBorrow = _toBorrow;
-
     }
 
 
@@ -45,15 +47,19 @@ contract FlashLoanBase is IFlashLoanSimpleReceiver {
     function updateAsset(IERC20 newAsset) external onlyAdmin {
         toBorrow = newAsset;
     }
+
     function updateOwner(address newOwner) external onlyOwner {
         owner = newOwner;
         admins[msg.sender] = false;
         admins[newOwner] = true;
     }
+
     function flipAdmin(address admin) external onlyOwner {
         admins[admin] = !admins[admin];
     }
+
 // --------------------------------------------------------------------------------- REQUEST ------------------------------------------------------------------------------------------
+
     // Override to add checks or logic if wanted. Encoded params might be passed to flashLoanLogic here.
     function requestFlashLoan(uint256 amount, bytes calldata params) public onlyAdmin virtual {
         POOL.flashLoanSimple(address(this), address(toBorrow), amount, params, 0);
@@ -95,12 +101,13 @@ contract FlashLoanBase is IFlashLoanSimpleReceiver {
 
 
 // --------------------------------------------------------------------------------- WITHDRAW -----------------------------------------------------------------------------------------
+
     function withdrawERC20(address asset) external onlyOwner {
         IERC20(asset).transfer(msg.sender, IERC20(asset).balanceOf(address(this)));
     }
+
     function withdrawETH() external onlyOwner {
         (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(success);
     }
-  
-  }
+}
